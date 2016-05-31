8418930f267c2ee15eee16b132d1a84733d82739
Hannu Rytkönen <hannu.rytkonen@gmail.com>
2016-05-31 22:11:52 +0300

    XOR for UI

--- src/Examples.jsx
+++ src/Examples.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import Perceptron from './Perceptron';
+import { Trainer } from 'synaptic';
 
 export default class Examples extends Component {
+  constructor(props) {
+    super(props);
+    let myPerceptron = new Perceptron(2,13,1);
+	myPerceptron.connectLayers();
+	const myTrainer = new Trainer(myPerceptron);
+	const trainer = myTrainer.XOR();
+    this.state = {
+    	perceptron: myPerceptron,
+    };
+  }
   render() {
     return (
-    <div>
-      <h1>Synaptic examples</h1>
-     </div>
+	    <div>
+	      <h1>Synaptic examples</h1>
+	      	<div>{this.state.perceptron.activate([0,0])}</div><div></div>
+	      	<div>{this.state.perceptron.activate([1,0])}</div><div></div>
+	      	<div>{this.state.perceptron.activate([0,1])}</div><div></div>
+	      	<div>{this.state.perceptron.activate([1,1])}</div><div></div>
+	    </div>
     );
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,21 +1,8 @@
-import Perceptron from './Perceptron';
-import { Trainer } from 'synaptic';
 import React from 'react';
 import { render } from 'react-dom';
 import Examples from './Examples';
  
-let myPerceptron = new Perceptron(2,13,1);
-myPerceptron.connectLayers();
-const myTrainer = new Trainer(myPerceptron);
- 
-console.log(myTrainer.XOR());
 
-console.log('--');
-console.log('0 ,0: ' + myPerceptron.activate([0,0]));
-console.log('1 ,0: ' + myPerceptron.activate([1,0]));
-console.log('0 ,1: ' + myPerceptron.activate([0,1]));
-console.log('1 ,1: ' + myPerceptron.activate([1,1]));	
-console.log('--');
 
 render(
   <Examples />,
