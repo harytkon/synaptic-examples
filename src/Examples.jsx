897d606a507672bd7645b9dfd8ca83f2285bb80e
Hannu Rytkönen <hannu.rytkonen@gmail.com>
2016-05-31 21:13:25 +0300

    added files

---
+++ src/Examples.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Examples extends Component {
+  render() {
+    return (
+    <div>
+      <h1>Synaptic examples</h1>
+     </div>
+    );
+  }
+}
